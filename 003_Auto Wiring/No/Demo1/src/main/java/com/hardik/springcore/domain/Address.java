db1ab11db5889a6e06a2a2a9bcb51008dfbac5c0
hardikhirapara91 <hardikhirapara91@gmail.com>
2017-08-19 14:22:04 +0530

    SpringCore auto-wiring by no example using xml configuration.

---
+++ 003_Auto Wiring/No/Demo1/src/main/java/com/hardik/springcore/domain/Address.java
@@ -0,0 +1,34 @@
+package com.hardik.springcore.domain;
+
+/**
+ * Application User
+ * 
+ * @author HARDIK HIRAPARA
+ *
+ */
+public class Address {
+
+	private String Street;
+	private String city;
+
+	public String getStreet() {
+		return Street;
+	}
+
+	public void setStreet(String street) {
+		Street = street;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	@Override
+	public String toString() {
+		return "Address [street=" + getStreet() + ", city=" + getCity() + "]";
+	}
+}
